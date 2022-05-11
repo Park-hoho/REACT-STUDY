dfd0fb7475e498730aab2f351c0eea693bef0e77
ByungHo <qudgh4290@gmail.com>
2022-05-11 13:13:54 +0900

    nextjs : add postcss, tailwind config

---
+++ next-js/company-website/components/masthead.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from "next/image";
+
+const Masthead: React.FC = () => {
+  return (
+    // https://www.youtube.com/watch?v=GznmPACXBlY 9:32
+    <div className="min-h-screen flex flex-col items-center justify-center">Masthead</div>
+  );
+}
+
+export default Masthead;
